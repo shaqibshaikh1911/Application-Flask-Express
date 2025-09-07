0b10adc8a3e2f012b31ecfda7dfa553bd793233c
shaqibshaikh1911 <shaqibshaikh1911@gmail.com>
2025-09-07 23:46:30 +0530

    Delete 02-backend directory

--- 02-backend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/')
-def get_message():
-    return jsonify({"message": "Code today, conquer tomorrow – every line brings you closer to your vision."})
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
